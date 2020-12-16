ab007c3dae3548c34cbfad46302be3a3bce52300
James Carpenter <james.edward.carpenter@gmail.com>
2020-12-16 07:22:06 -0800

    Adjusted functions in function.py and imported all functions in init

--- molecool/functions.py
+++ molecool/functions.py
@@ -5,6 +5,11 @@ A Python package for analyzing and visualizing xyz files.
 Handles the primary functions
 """
 
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
 
 def canvas(with_attribution=True):
     """
@@ -27,14 +32,6 @@ def canvas(with_attribution=True):
     if with_attribution:
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
-
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-
-from mpl_toolkits.mplot3d import Axes3D
-
-#%matplotlib notebook
 
 def calculate_distance(rA, rB):
     # This function calculates the distance between two points given as numpy arrays.
