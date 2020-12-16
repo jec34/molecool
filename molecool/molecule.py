b118cc16a961d49ee34806672470d032ec629f4b
James Carpenter <james.edward.carpenter@gmail.com>
2020-12-16 15:01:29 -0800

    Add COM function and test

--- molecool/molecule.py
+++ molecool/molecule.py
@@ -4,6 +4,7 @@ Functions for analying/measuring molecules.
 
 from .measure import calculate_distance
 from .atom_data import atomic_weights
+import numpy as np
 
 def build_bond_list(coordinates, max_bond=1.5, min_bond=0):
 
@@ -36,3 +37,39 @@ def calculate_molecular_mass(symbols):
    for symbol in symbols:
        mass += atomic_weights[symbol]
    return mass
+
+def calculate_center_of_mass(symbols, coordinates):
+   """Calculate the center of mass of a molecule.
+
+   The center of mass is weighted by each atom's weight.
+
+   Parameters
+   ----------
+   symbols : list
+       A list of elements for the molecule
+   coordinates : np.ndarray
+       The coordinates of the molecule.
+
+   Returns
+   -------
+   center_of_mass: np.ndarray
+       The center of mass of the molecule.
+
+   Notes
+   -----
+   The center of mass is calculated with the formula
+
+   .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+
+   """
+
+   total_mass = calculate_molecular_mass(symbols)
+
+   mass_array = np.zeros([len(symbols), 1])
+
+   for i in range(len(symbols)):
+       mass_array[i] = atomic_weights[symbols[i]]
+
+   center_of_mass = sum(coordinates * mass_array) / total_mass
+
+   return center_of_mass
